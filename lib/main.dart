d9728d6083299158c4d998d12ff3b765783b46fd
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-05 10:32:50 -0600

    prueba commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: Scaffold( 
         body: Center(
           child: Text('Hello World!'),
         ),
