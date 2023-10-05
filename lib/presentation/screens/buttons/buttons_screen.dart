941f997c1d441318b2a48454684745f736242f90
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-05 13:54:04 -0600

    Creación de botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -10,7 +11,59 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon( Icons.arrow_back_ios_new_rounded),
+        onPressed: (){
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: const Text('Elevated')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Disable')),
+            ElevatedButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.access_time_filled),
+              label: const Text('Elevated Icon'),
+            ),
+            FilledButton(onPressed: (){}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: (){}, 
+              label: const Text('Filled Icon'), 
+              icon: const Icon(Icons.add_chart_sharp)
+            ),
+            OutlinedButton(onPressed: (){}, child: const Text('Outline')),
+            OutlinedButton.icon(
+              onPressed: (){},
+              label: const Text('Outline icon'),
+              icon: const Icon(Icons.hexagon),
+            ),
+            TextButton(onPressed: (){}, child: const Text('Text')),
+            TextButton.icon(
+              onPressed: (){},
+              label: const Text('Text icon'),
+              icon: const Icon(Icons.hexagon),
+            ),
+            IconButton(onPressed: (){}, icon: const Icon(Icons.pin_outlined)),
+          ],
+        ),
+      ),
     );
   }
 }
