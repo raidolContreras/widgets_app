4ee30d9cab33f165266f15e65a085c2e8bac4fc6
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-05 14:03:20 -0600

    Boton personalizado

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -61,7 +61,36 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.hexagon),
             ),
             IconButton(onPressed: (){}, icon: const Icon(Icons.pin_outlined)),
+
+            const CustomButton()
           ],
+        ),
+      ),
+    );
+  }
+}
+// boton personalizado
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {
+            
+          },
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+            child: Text('Personalizado', style: TextStyle(color: Colors.white),)
+          ),
         ),
       ),
     );
