4feda95c329ffcd6d35a690f6827dfeb4e70bff1
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-24 12:04:55 -0600

    Sentencia de recuperación de reglamentos

--- lib/presentation/screens/reglaments/reglaments_screen.dart
+++ lib/presentation/screens/reglaments/reglaments_screen.dart
@@ -1,40 +1,113 @@
 import 'package:flutter/material.dart';
 import '/presentation/widgets/side_menu.dart';
+import 'package:in_library/config/mysql/mysql.dart';
+import 'package:card_swiper/card_swiper.dart';
 
-
-class ReglamentsScreen extends StatelessWidget {
+class ReglamentsScreen extends StatefulWidget {
   static const name = 'reglaments_screen';
   const ReglamentsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final scaffoldKey = GlobalKey<ScaffoldState>();
+  ReglamentsScreenState createState() => ReglamentsScreenState();
+}
 
+class ReglamentsScreenState extends State<ReglamentsScreen> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  var db = MySql();
+  List reglament = [];
+  List idReglament = [];
+  List cover = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getReglaments();
+  }
+
+  void getReglaments() {
+    db.getConnection().then((conn) {
+
+      String sql = "SELECT * FROM app_titles t LEFT JOIN app_covers ac ON t.idTitles = ac.Title_idTitles WHERE t.type_title = 'Reglamento'";
+      conn.query(sql).then((results) {
+        for(var row in results){
+          setState(() {
+            idReglament.add(row[0]);
+            reglament.add(row[1]);
+            cover.add(row[6]);
+          });
+        }
+      });
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _ViewReglaments(scaffoldKey: scaffoldKey, idReglament: idReglament, reglament: reglament, cover: cover);
+  }
+}
+
+class _ViewReglaments extends StatelessWidget {
+  const _ViewReglaments({
+    required this.scaffoldKey,
+    required this.idReglament,
+    required this.reglament,
+    required this.cover,
+  });
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final List reglament;
+  final List idReglament;
+  final List cover;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Reglamentos'),
       ),
       endDrawer: SideMenu(scaffoldKey: scaffoldKey), // Utiliza endDrawer en lugar de drawer
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Agrega aquí la funcionalidad para la opción 1
+
+      body: Column(
+        children: [
+          const SizedBox(height: 40),
+          const SizedBox(height: 40,
+            child: Text('Selecciona un reglamento', style: TextStyle(fontSize: 25),)
+          ),
+          Center(
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Column(
+                    children: [
+                    const SizedBox(height: 20),
+                      AspectRatio(
+                        aspectRatio: 0.9,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: (cover[index] != null)
+                                  ?Image.network('https://app-fiscal.inscripcionesccm.online/assets/images/covers/${idReglament[index]}/${cover[index]}', 
+                                    fit: BoxFit.fill,
+                                  )
+                                  :Image.network('https://publications.iarc.fr/uploads/media/default/0001/02/thumb_1244_default_publication.jpeg', 
+                                    fit: BoxFit.contain,
+                                  ),
+                        )
+                      ),
+                      const SizedBox(height: 40,),
+                      Text('${reglament[index]}', style: const TextStyle(fontSize: 20),),
+                    ],
+                  ),
+                );
               },
-              child: const Text('Opción 1'),
+              itemCount: reglament.length,
+              itemWidth: MediaQuery.of(context).size.width * 0.9,
+              itemHeight: MediaQuery.of(context).size.height * 0.6,
+              layout: SwiperLayout.TINDER,
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Agrega aquí la funcionalidad para la opción 2
-              },
-              child: const Text('Opción 2'),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
