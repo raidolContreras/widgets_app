b10da5d50c08e87686d58d97bdefe7b80ac34ba1
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-26 09:10:53 -0600

    impresión de capitulos

--- lib/models/statements_models.dart
+++ lib/models/statements_models.dart
@@ -4,5 +4,7 @@ class StatementsModels {
   static const String getReglament = "SELECT * FROM app_titles t LEFT JOIN app_covers ac ON t.idTitles = ac.Title_idTitles WHERE t.idTitles = ?";
 
   static const String getChapters = "SELECT * FROM app_chapter WHERE Title_idTitles = ?";
-  
+
+  static const String getSections = "SELECT * FROM app_sections WHERE Chapter_idChapters = ?";
+
 }
--- lib/presentation/screens/reglament/reglament_screen.dart
+++ lib/presentation/screens/reglament/reglament_screen.dart
@@ -1,8 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:in_library/config/mysql/mysql.dart';
-import 'package:in_library/models/statements_models.dart';
 import '/presentation/widgets/side_menu.dart';
+import 'package:in_library/config/mysql/mysql.dart';
 
 class ReglamentScreen extends StatefulWidget {
   static const name = 'reglament_screen';
@@ -15,42 +14,47 @@ class ReglamentScreen extends StatefulWidget {
 class _ReglamentScreenState extends State<ReglamentScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   var db = MySql();
-  String nameReglament = '';
-  String cover = '';
-  List<String> chapters = [];
   int idReglament = 0;
+  String cover = '';
+  Map reglament = {};
+  Map chapters = {};
   bool isLoading = true;
-  ScrollController _scrollController = ScrollController();
-  bool _isLoadingMore = false;
 
   @override
   void initState() {
     super.initState();
+  }
 
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
-        
-      }
-    });
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     getReglament();
-    getChapters();
+    getChapters(); // Llama a la función para cargar los capítulos
   }
 
   void getReglament() {
     final arguments = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    idReglament = int.parse(arguments['idReglament']!);
+    idReglament = int.parse(arguments['idReglament']!); // Obtiene el ID
 
     db.getConnection().then((conn) {
-      conn.query(StatementsModels.getReglament, [idReglament]).then((results) {
-        for (var row in results) {
+      String titles = "SELECT * FROM app_titles t LEFT JOIN app_covers ac ON t.idTitles = ac.Title_idTitles WHERE t.idTitles = ?";
+      conn.query(titles, [idReglament]).then((results) {
+        if (results.isNotEmpty) {
+          final row = results.first;
           setState(() {
-            nameReglament = row[1].toString();
             cover = row[6].toString();
+            reglament = {
+              'id': row[0],
+              'nombre': row[1].toString(),
+              'cover': (row[3] != null)
+                  ? 'https://app-fiscal.inscripcionesccm.online/assets/images/covers/$idReglament/$cover'
+                  : 'https://publications.iarc.fr/uploads/media/default/0001/02/thumb_1244_default_publication.jpeg',
+            };
             isLoading = false;
           });
         }
@@ -59,14 +63,22 @@ class _ReglamentScreenState extends State<ReglamentScreen> {
   }
 
   void getChapters() {
+    final arguments = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    idReglament = int.parse(arguments['idReglament']!); // Obtiene el ID
+
     db.getConnection().then((conn) {
-      conn.query(StatementsModels.getChapters, [idReglament]).then((results) {
+      String chaptersSql = "SELECT * FROM app_chapter WHERE Title_idTitles = ?";
+      conn.query(chaptersSql, [idReglament]).then((results) {
         for (var row in results) {
           setState(() {
-            chapters.add(row[1].toString());
-            isLoading = false;
+            chapters[row[0]] = {
+              'idChapter': row[0],
+              'nameChapter': row[1],
+            };
           });
+ 
         }
+        print('data $chapters');
       });
     });
   }
@@ -87,56 +99,66 @@ class _ReglamentScreenState extends State<ReglamentScreen> {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
-        title: Center(child: Text(nameReglament)),
+        title: Center(
+            child: FadeIn(
+                duration: const Duration(milliseconds: 200),
+                child: Text(reglament['nombre'] ?? ''))),
       ),
       endDrawer: SideMenu(scaffoldKey: scaffoldKey),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 20),
-              child: Column(
-                children: [
-                  FadeInDown(
-                    child: DecoratedBox(
-                      decoration: decoration,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.network(
-                          (cover != '')
-                              ? 'https://app-fiscal.inscripcionesccm.online/assets/images/covers/$idReglament/$cover'
-                              : 'https://publications.iarc.fr/uploads/media/default/0001/02/thumb_1244_default_publication.jpeg',
-                          fit: BoxFit.fill,
+          : ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+                  child: Column(
+                    children: [
+                      FadeInDown(
+                        duration: const Duration(seconds: 1),
+                        child: DecoratedBox(
+                          decoration: decoration,
+                          child: SizedBox(
+                            height: 200,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.network(
+                                reglament['cover'],
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 20,),
-                  FadeInUp(
-                    child: Center(
-                      child: Text(
-                        nameReglament,
-                        style: const TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                      const SizedBox(height: 20,),
+                      FadeInUp(
+                        child: Center(
+                          child: Text(
+                            reglament['nombre'],
+                            style: const TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 10,),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: chapters.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final chapter = chapters[index + 1];
+                          return Center(
+                            child: Text(chapter['nameChapter'].toString(), // Convierte a cadena de texto
+                            style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),),
+                          );
+                        },
+                      )
+                    ],
                   ),
-                  Expanded(
-                    child: ListView.builder(
-                      controller: _scrollController,
-                      itemCount: chapters.length + 1,
-                      itemBuilder: (BuildContext context, int index) {
-                        if (index == chapters.length) {
-                          if (_isLoadingMore) {
-                            return Center(child: CircularProgressIndicator());
-                          }
-                        } else {
-                          return Text(chapters[index]);
-                        }
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                ),
+              ],
+            )
+
     );
   }
 }
