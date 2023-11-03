9bee7d8fa14a846bd429938d3da7b26090d37cf5
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-03 09:53:18 -0600

    selectUser para el Login

---
+++ lib/domain/entities/select_user.dart
@@ -0,0 +1,21 @@
+class SelectUser {
+  
+  final int idUser;
+  final String firstname;
+  final String lastname;
+  final String birthday;
+  final String email;
+  final String phone;
+  final String creationDate;
+
+  SelectUser({
+    required this.idUser,
+    required this.firstname,
+    required this.lastname,
+    required this.birthday,
+    required this.email,
+    required this.phone,
+    required this.creationDate, 
+  });
+
+}
