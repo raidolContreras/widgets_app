9541e91b09a3624772b43f766d7b7c6f067d8a67
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-03 16:08:09 -0600

    local storage, datasource and repository

---
+++ lib/domain/repositories/local_storage_repository.dart
@@ -0,0 +1,10 @@
+
+import 'package:in_library/domain/entities/select_user.dart';
+
+abstract class LocalStorageRepository{
+
+  Future<void> toogleLogin(SelectUser user);
+
+  Future<bool> islogged(int userId);
+
+}
