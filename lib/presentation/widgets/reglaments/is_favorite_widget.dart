de8eeb332f6f0a80a9c954f327ef2a49d5f7ed80
raidolContreras <oscarcontrerasf91@gmail.com>
2023-12-06 09:37:31 -0600

    mejoras

--- lib/presentation/widgets/reglaments/is_favorite_widget.dart
+++ lib/presentation/widgets/reglaments/is_favorite_widget.dart
@@ -5,12 +5,14 @@ import 'package:in_library/domain/entities/add_favorites.dart';
 import 'package:in_library/infrastructure/datasources/favoritesdb_datasource.dart';
 import 'package:in_library/presentation/providers/add_favorites/add_favorites_provider.dart';
 
+import 'dart:async';
+
 class IsFavorite extends ConsumerStatefulWidget {
   const IsFavorite({
-    super.key, 
+    Key? key,
     required this.idArticle,
     required this.userId,
-  });
+  }) : super(key: key);
 
   final String idArticle;
   final String userId;
@@ -20,39 +22,60 @@ class IsFavorite extends ConsumerStatefulWidget {
 }
 
 class IsFavoriteState extends ConsumerState<IsFavorite> {
-
   late IsFavorites isFavorite;
   bool star = false;
   Icon favorite = const Icon(Icons.star_border_outlined);
 
+  @override
+  void initState() {
+    super.initState();
+    _initializeFavoritesChain();
+  }
+
+  Future<void> _initializeFavoritesChain() async {
+    await _delayedIsFavorite(500); // Ajusta el valor del delay según tus necesidades
+    await _delayedIsFavorite(500); // Ajusta el valor del delay según tus necesidades
+    // Agrega más llamadas a _delayedIsFavorite según sea necesario
+  }
+
+  Future<void> _delayedIsFavorite(int delay) async {
+    await Future.delayed(Duration(milliseconds: delay));
+    await _isFavorite();
+  }
+
   Future<void> _isFavorite() async {
     final isFavoritedbDatasource = FavoritesbdDatasource();
-    isFavorite = await isFavoritedbDatasource.isFavorite(widget.idArticle, widget.userId);
-    
+    isFavorite =
+        await isFavoritedbDatasource.isFavorite(widget.idArticle, widget.userId);
     setState(() {
       star = isFavorite.message;
     });
-    if(star){
-      favorite = const Icon(Icons.star_rounded);
-    }else {
+    // ignore: use_build_context_synchronously
+    final colors = Theme.of(context).colorScheme;
+    if (star) {
+      favorite = Icon(Icons.star_rounded, color: colors.primary);
+    } else {
       favorite = const Icon(Icons.star_border_outlined);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    _isFavorite();
+    final colors = Theme.of(context).colorScheme;
     return IconButton(
       onPressed: () async {
-        ref.read(toggleFavorites.notifier).fetchToggleFavorites(widget.idArticle, widget.userId);
+        ref.read(toggleFavorites.notifier)
+            .fetchToggleFavorites(widget.idArticle, widget.userId);
         setState(() {
-          if(star){
+          if (star) {
             favorite = const Icon(Icons.star_border_outlined);
+            star = !star;
           } else {
-            favorite = const Icon(Icons.star_rounded);
+            favorite = Icon(Icons.star_rounded, color: colors.primary);
+            star = !star;
           }
         });
-      }, 
+      },
       icon: ElasticInDown(child: favorite),
     );
   }
